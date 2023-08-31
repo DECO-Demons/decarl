4acc1116a1a3750b0fdcb0c19e7be2159e6efb1e
Alex Janevik <ajanevik@gmail.com>
2023-08-31 18:21:49 +1000

    feat: adds scrollable page view

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'dart:io' show Platform;
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'screens/map.dart' show MapPage;
+import 'screens/home.dart' show HomePage;
 
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatefulWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   State<MainApp> createState() => _MainAppState();
 }
 
 class _MainAppState extends State<MainApp> {
-  late String mapStyleAndroid;
-  late String mapStyleIos;
+  final _pageController = PageController(
+    // Index of home screen
+    initialPage: 0,
+  );
 
   @override
-  void initState() {
-    // Map style for Android must load from .txt, for iOS from .json
-    rootBundle.loadString('assets/map_style.txt').then((string) {
-      mapStyleAndroid = string;
-    });
-    rootBundle.loadString('assets/map_style.json').then((string) {
-      mapStyleIos = string;
-    });
-    super.initState();
-  }
-
-  late GoogleMapController mapController;
-  // Default location
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-    Platform.isAndroid
-        ? controller.setMapStyle(mapStyleAndroid)
-        : controller.setMapStyle(mapStyleIos);
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Decarl maps implementation test'),
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
+          appBar: AppBar(
+            title: const Center(child: Text('Decarl Test')),
           ),
-        ),
-      ),
+          body: PageView(
+            controller: _pageController,
+            scrollDirection: Axis.horizontal,
+            // All pages
+            children: const [
+              HomePage(),
+              MapPage(),
+            ],
+          )),
     );
   }
 }
