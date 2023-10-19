94e0cb332d793aaadd5c9842635c52d134b84f73
Luka Manitta <luka@lukamanitta.com>
2023-10-19 11:11:19 +1000

    feat(wip): reduce to art retrievals only

--- lib/data_classes/Art.dart
+++ lib/data_classes/Art.dart
@@ -1,34 +1,33 @@
 class Art {
-    final String? id;
+    final String anchor_id;
     final String user_id;
-    final Timestamp time_created; // Figure out type for this
-    final Latlon lat_lon; // Also for this
-    final Blob image_blob; // And this
-    // final int likes_count; // May not implement
-    // final List<String>? like_ids; // May not implement
-    // final List<String>? comment_ids; // May not implement
+    final int time_created;
+    final String lat_lon; // Also for this
+    final int likes_count; // May not implement
 
     Art({
-        this.id,
+        required this.anchor_id,
         required this.user_id,
         required this.time_created,
         required this.lat_lon,
-        required this.image_blob
+        required this.likes_count
     });
 
     Map<String, dynamic> toMap() {
         return {
-            "user_id": user_id,
-            "time_created": time_created, // convert to string
-            "lat_lon": lat_lon, // convert to string
-            "image_blob": image_blob // whatever format this will be
+            anchor_id.toString(): {
+                "user_id": user_id,
+                "time_created": time_created, // convert to string
+                "lat_lon": lat_lon, // convert to string
+                "likes_count": this.likes_count
+            }
         };
     }
 
     Art.fromMap(Map<String, dynamic> artData)
-        : id = artData["id"], // will be root node
-          user_id = artData["user_id"]
-          time_created = artData["time_created"], // Convert to whatever type
+        : anchor_id = artData.keys.elementAt(0),
+          user_id = artData["user_id"],
+          time_created = artData["time_created"],
           lat_lon = artData["lat_lon"], // Also convert
-          image_blob = artData["image_blob"]; // also convert
+          likes_count = artData["likes_count"];
 }
--- lib/database_service.dart
+++ lib/database_service.dart
@@ -1,31 +1,10 @@
+import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:geoflutterfire2/geoflutterfire2.dart';
+import 'data_classes/Art.dart';
+
 class DatabaseService {
     final DatabaseReference _db = FirebaseDatabase.instance.ref(); // not correct? use realtimedatabase
-
-    DatabaseReference getUserRef(String userId) async {
-        return await _db.child("users/$userId").ref();
-    }
-
-    addUser(User userData) async {
-        // https://stackoverflow.com/a/67313263
-        final pushRef = await _db.child("users").push(userData.toMap());
-        userData.id = pushRef.key;
-    }
-
-    updateUser(String userId, User newUserData) async {
-        final DatabaseReference userRef = await getUserRef(userId);
-        // use ref.set({}), need to merge values somehow
-    }
-
-    addArt(Art artObject) async {
-        final pushRef = await _db.child("art").push(artObject.toMap());
-        artObject.id = pushRef.key;
-    }
-
-    Future<List<User>> retrieveUsers() async {
-        final usersData = _db.child("users").get();
-        final List<User> userObjects = ...;
-        // Convert children to User & add to list
-    }
-
-    // Can put more custom queries here
 }
